6c73dc404e5d67e260a3cf84a1ab17e819d73387
cheenaze <oqaymoments@gmail.com>
2022-12-12 01:27:55 +0300

    Update memory.hpp

--- memory.hpp
+++ memory.hpp
@@ -2,6 +2,7 @@
 
 #include <string_view>
 
+namespace memory {
 template <typename T, typename U>
 void printAs(T* t) {
     constexpr size_t size = sizeof(T) / sizeof(U);
@@ -29,6 +30,20 @@ U* findOffset(T* where, U value) {
     return retPtr;
 }
 
+template <typename U>
+U* findOffset(std::byte* searchFrom, U value, size_t sizeInBytes) {
+    const size_t size = sizeInBytes / sizeof(U);
+    U* u = reinterpret_cast<U*>(searchFrom);
+    U* retPtr;
+    for (auto idx = 0; idx < size; idx++) {
+        if (u[idx] == value) {
+            retPtr = (u + idx);
+            break;
+        }
+    }
+    return retPtr;
+}
+
 template <typename T>
 auto* findOffset(T* where, std::string_view value) {
     constexpr size_t size = value.size();
@@ -36,7 +51,7 @@ auto* findOffset(T* where, std::string_view value) {
     char* retPtr;
 
     for (auto idx = 0; idx < size; idx++) {
-        if (std::string_view{u[idx], u[idx] + size} == value) {
+        if (std::string_view{u[idx], size} == value) {
             retPtr = (u + idx);
             break;
         }
@@ -51,3 +66,4 @@ ptrdiff_t getDiff(T* t, U* u) {
 
     return ptrdiff_t(v2 - v1);
 }
+}  // namespace memory
