858b92a295356becbcc07a54a37971124fb1aaa4
cheenaze <oqaymoments@gmail.com>
2023-06-23 15:33:17 +0300

    inline initialization, formatting

--- Random.hpp
+++ Random.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include <random>
 
-template <typename T = int64_t>
+template <typename T = uint32_t>
 class Random {
    private:
-    static std::mt19937 gen;
+    static inline std::mt19937 gen{std::random_device{}()};
 
    public:
     Random() = default;
@@ -12,21 +12,15 @@ class Random {
     Random(const Random&) = delete;
     Random& operator=(const Random&) = delete;
 
-    [[nodiscard]] static T
-    get() {
+    [[nodiscard]] static T get() {
         return std::uniform_int_distribution()(gen);
     }
 
-    [[nodiscard]] static T
-    get(T maxN) {
+    [[nodiscard]] static T get(T maxN) {
         return std::uniform_int_distribution(T{}, maxN)(gen);
     }
 
-    [[nodiscard]] static T
-    get(T minN, T maxN) {
+    [[nodiscard]] static T get(T minN, T maxN) {
         return std::uniform_int_distribution(minN, maxN)(gen);
     }
 };
-
-template <typename T>
-std::mt19937 Random<T>::gen{std::random_device{}()};
