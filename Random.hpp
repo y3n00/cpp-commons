e65bf3eb1c630f4f35c52aad81db4f7462bb8486
y3n00 <81647544+y3n00@users.noreply.github.com>
2024-02-08 14:02:38 +0300

    small changes

--- Random.hpp
+++ Random.hpp
@@ -1,8 +1,12 @@
 #pragma once
+#include <limits>
 #include <random>
+#include <string>
 
-template <typename T = uint32_t>
+template <std::integral Ty = uint32_t>
 class Random {
+    using typeLimit = std::numeric_limits<Ty>;
+
    private:
     static inline std::mt19937 gen{std::random_device{}()};
 
@@ -12,15 +16,18 @@ class Random {
     Random(const Random&) = delete;
     Random& operator=(const Random&) = delete;
 
-    [[nodiscard]] static T get() {
-        return std::uniform_int_distribution()(gen);
+    [[nodiscard]] static inline Ty get(Ty min = typeLimit::min(), Ty max = typeLimit::max()) {
+        return std::uniform_int_distribution(min, max)(gen);
     }
 
-    [[nodiscard]] static T get(T maxN) {
-        return std::uniform_int_distribution(T{}, maxN)(gen);
-    }
-
-    [[nodiscard]] static T get(T minN, T maxN) {
-        return std::uniform_int_distribution(minN, maxN)(gen);
+    [[nodiscard]] static inline std::string generate_string(size_t strLen) {
+        constexpr static std::string_view SYMBOLS =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "1234567890";
+        std::string result(strLen, ' ');
+        for (char& ch : result)
+            ch = SYMBOLS[get(0, SYMBOLS.length() - 1)];
+        return result;
     }
 };
