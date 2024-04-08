6b448a72d5102106d41bb08c0e63f7e8c0987483
y3n00 <81647544+y3n00@users.noreply.github.com>
2024-04-09 00:03:15 +0300

    [+] cpp std check, [+] vector of colors, [+] new colors

--- console_colors.hpp
+++ console_colors.hpp
@@ -1,40 +1,75 @@
 #pragma once
 
-#if defined(__cplusplus) && (__cplusplus >= 202002L)
+#if defined(_MSVC_LANG)
+#define VT_CPP_VERSION _MSVC_LANG
+#else
+#define VT_CPP_VERSION __cplusplus
+#endif
+
+#if (VT_CPP_VERSION >= 202002L)
 #include <format>
 namespace _FMT = std;
 #else
-#include <fmt/format-inl.h>
+#include <fmt/format.h>
 namespace _FMT = fmt;
 #endif
 
 #include <string>
-
+#include <vector>
 namespace cliColors {
     enum class Colors {
-        red,
-        orange,
-        yellow,
-        green,
-        cyan,
-        blue,
-        purple,
-        pink,
-        brightgreen,
-        brightred,
-        black,
-        gray,
-        brightgray,
-        white,
-        reset,
-        _default,
+        reset = 0,
+        black = 30,
+        red = 31,
+        green = 32,
+        yellow = 33,
+        orange = 33,
+        blue = 34,
+        purple = 35,
+        pink = 35,
+        cyan = 36,
+        gray = 37,
+        brightgray = 90,
+        darkgray = 90,
+        brightred = 91,
+        brightgreen = 92,
+        lightblue = 94,
+        magenta = 95,
+        lightcyan = 96,
+        white = 97,
+        _default = 39,
     };
+
+    static inline std::vector<Colors> getAllColors() {
+        static const auto colors = std::vector{
+            Colors::reset,
+            Colors::black,
+            Colors::red,
+            Colors::green,
+            Colors::yellow,
+            Colors::orange,
+            Colors::blue,
+            Colors::purple,
+            Colors::pink,
+            Colors::cyan,
+            Colors::gray,
+            Colors::brightgray,
+            Colors::darkgray,
+            Colors::brightred,
+            Colors::brightgreen,
+            Colors::lightblue,
+            Colors::magenta,
+            Colors::lightcyan,
+            Colors::white,
+            Colors::_default,
+        };
+        return colors;
+    }
 
     struct ColorTxt {
         static inline std::string Colorize(const std::string& str, Colors c) {
-            return _FMT::format("\x1b[1;{}m{}{}", (static_cast<int>(c) + 30), str, Reset());
+            return _FMT::format("\x1b[1;{}m{}{}", static_cast<int>(c), str, Reset());
         }
-
         static inline std::string Reset() { return "\x1b[0m"; }
     };
 };  // namespace cliColors
