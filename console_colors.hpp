6f9975dcf3b3b519364d8dc6be216b9ea958c811
y3n00 <81647544+y3n00@users.noreply.github.com>
2024-07-04 14:12:22 +0300

    duplicate colors have been removed

--- console_colors.hpp
+++ console_colors.hpp
@@ -24,14 +24,11 @@ namespace cliColors {
         red = 31,
         green = 32,
         yellow = 33,
-        orange = 33,
         blue = 34,
         purple = 35,
-        pink = 35,
         cyan = 36,
         gray = 37,
         brightgray = 90,
-        darkgray = 90,
         brightred = 91,
         brightgreen = 92,
         lightblue = 94,
@@ -48,14 +45,11 @@ namespace cliColors {
             Colors::red,
             Colors::green,
             Colors::yellow,
-            Colors::orange,
             Colors::blue,
             Colors::purple,
-            Colors::pink,
             Colors::cyan,
             Colors::gray,
             Colors::brightgray,
-            Colors::darkgray,
             Colors::brightred,
             Colors::brightgreen,
             Colors::lightblue,
@@ -68,10 +62,10 @@ namespace cliColors {
     }
 
     namespace ColorTxt {
-        [[nodiscard]] inline std::string Reset() { return "\x1b[0m"; }
+        [[nodiscard]] constexpr auto Reset() { return std::string_view{"\x1b[0m"}; }
 
         [[nodiscard]] std::string Colorize(const std::string& str, Colors c) {
-            return _FMT::format("\x1b[1;{}m{}{}", static_cast<int>(c), str, Reset());
+            return _FMT::format("\x1b[1;{}m{}{}", std::to_underlying(c), str, Reset());
         }
     }  // namespace ColorTxt
 };  // namespace cliColors
