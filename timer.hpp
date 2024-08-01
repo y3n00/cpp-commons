ab2bcab8583d6f5b331b54c15adeacc9be33855d
y3n00 <81647544+y3n00@users.noreply.github.com>
2024-08-02 02:29:39 +0300

    rule of 5, noexcept tag

--- timer.hpp
+++ timer.hpp
@@ -26,51 +26,41 @@ class Timer {
 
    public:
     Timer() = default;
+    Timer(const Timer& other) = default;
+    Timer(Timer&& other) noexcept = default;
+    Timer& operator=(const Timer& other) = default;
+    Timer& operator=(Timer&& other) noexcept = default;
 
-    Timer(Timer&& other) noexcept
-        : is_running{std::move(other.is_running)},
-          m_start{std::move(other.m_start)},
-          m_stop{std::move(other.m_stop)},
-          m_timestamps{std::move(other.m_timestamps)} {
-        other.is_running = false;
-    }
+    [[nodiscard]] decltype(auto) start_timestamp() const noexcept { return m_start; }
 
-    Timer& operator=(Timer&& other) noexcept {
-        is_running = std::move(other.is_running);
-        m_start = std::move(other.m_start);
-        m_stop = std::move(other.m_stop);
-        m_timestamps = std::move(other.m_timestamps);
-        other.is_running = false;
-        return *this;
-    }
+    [[nodiscard]] decltype(auto) stop_timestamp() const noexcept { return m_stop; }
 
-    [[nodiscard]] auto start_timestamp() const { return m_start; }
-
-    [[nodiscard]] auto stop_timestamp() const { return m_stop; }
-
-    [[nodiscard]] auto all_timestamps() const {
+    [[nodiscard]] decltype(auto) all_timestamps() const {
         return m_timestamps |
                std::views::transform([](const auto& ns) { return std::chrono::duration_cast<M>(ns); }) |
                std::ranges::to<std::vector>();
     }
 
-    [[nodiscard]] auto get_duration() const {
+    [[nodiscard]] decltype(auto) get_duration() const noexcept {
         const auto duration = (is_running ? Clock_Type::now() : m_stop) - m_start;
         return std::chrono::duration<double, typename M::period>(duration);
     }
 
-    inline void reset() noexcept {
-        *this = Timer{};
+    void reset() noexcept {
+        is_running = false;
+        m_start = {};
+        m_stop = {};
+        m_timestamps.clear();
     }
 
-    void start() {
+    void start() noexcept {
         reset();
         m_timestamps.emplace_back(0);
         m_start = Clock_Type::now();
         is_running = true;
     }
 
-    void stop() {
+    void stop() noexcept {
         if (!is_running) [[unlikely]]
             return;
         timestamp();
@@ -78,7 +68,7 @@ class Timer {
         is_running = false;
     }
 
-    void timestamp() {
+    void timestamp() noexcept {
         if (!is_running) [[unlikely]]
             return;
         m_timestamps.emplace_back(Clock_Type::now() - m_start);
@@ -100,39 +90,31 @@ class BenchTimer {
         return m_timers[title] = Timer_t{};
     }
 
-    void start_all() {
-        constexpr auto start_f = [](auto& elem) { elem.start(); };
-        process_map(start_f);
+    void start_all() noexcept {
+        process_map([](auto& elem) { elem.start(); });
     }
 
-    void stop_all() {
-        constexpr auto stop_f = [](auto& elem) { elem.stop(); };
-        process_map(stop_f);
+    void stop_all() noexcept {
+        process_map([](auto& elem) { elem.stop(); });
     }
 
-    void make_timestamp(const std::string& title) {
+    void make_timestamp(const std::string& title) noexcept {
         auto it = m_timers.find(title);
-        if (it != m_timers.end()) [[unlikely]]
+        if (it != m_timers.end() && it->second.is_running)
             it->second.timestamp();
     }
 
-    [[nodiscard]] auto get_all() const { return m_timers; }
+    [[nodiscard]] decltype(auto) get_all() const noexcept { return m_timers; }
 
-    void remove(const std::string& title) { m_timers.erase(title); }
+    void remove(const std::string& title) noexcept { m_timers.erase(title); }
 
-    void remove_all() { m_timers.clear(); }
+    void remove_all() noexcept { m_timers.clear(); }
 
    private:
     TimerMap m_timers;
-    void process_map(auto&& func) {
-        if (m_timers.empty())
-            return;
-        const auto sz = m_timers.size(), threshold = 20;
+    void process_map(auto&& func) noexcept {
         auto&& timers = m_timers | std::views::values;
-        if (sz <= threshold) [[likely]]
-            std::ranges::for_each(timers, func);
-        else
-            std::for_each(std::execution::par, timers.begin(), timers.end(), func);
+        std::ranges::for_each(timers, func);
     }
 };
 
@@ -142,7 +124,7 @@ class Timer_Wrapper {
 
    public:
     explicit Timer_Wrapper(Timer_t& timer) noexcept : m_timer(timer) { m_timer.start(); }
-    ~Timer_Wrapper() { m_timer.stop(); }
+    ~Timer_Wrapper() noexcept { m_timer.stop(); }
 
     Timer_Wrapper(const Timer_Wrapper&) = delete;
     Timer_Wrapper& operator=(const Timer_Wrapper&) = delete;
