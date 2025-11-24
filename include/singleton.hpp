08d7f92f18b91921eb55460b5ca126806b1b0fb3
y3n00 <81647544+y3n00@users.noreply.github.com>
2025-11-24 22:30:56 +0300

    simplified no alloc singleton

--- include/singleton.hpp
+++ include/singleton.hpp
@@ -1,29 +1,22 @@
 #pragma once
 
-#include <memory>
-#include <mutex>
-
 template <typename T>
 class Singleton
 {
-  public:
-	Singleton(const Singleton&)			   = delete;
-	Singleton(Singleton&&)				   = delete;
-	Singleton& operator=(const Singleton&) = delete;
-	Singleton& operator=(Singleton&&)	   = delete;
+public:
+    Singleton(const Singleton&)            = delete;
+    Singleton(Singleton&&)                 = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    Singleton& operator=(Singleton&&)      = delete;
 
-	static T& get_instance()
-	{
-		std::call_once(init_flag, [] { instance = std::make_unique<T>(); });
+    static T& instance()
+    {
+        static T inst;
+		
+        return inst;
+    }
 
-		return *instance;
-	}
-
-  protected:
-	Singleton()	 = default;
-	~Singleton() = default;
-
-  private:
-	inline static std::unique_ptr<T> instance = nullptr;
-	inline static std::once_flag	 init_flag;
+protected:
+    Singleton()  = default;
+    ~Singleton() = default;
 };
